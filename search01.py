2842d8e7b8e976ce35ef2ef60f6946e05fbc58ec
MahanMehrvarz <mahan.mehrvarz@hotmail.com>
2016-03-28 01:27:04 -0400

    now can write to file

--- search01.py
+++ search01.py
@@ -1,15 +1,49 @@
 
-from keys import *
+import keys
 from twython import Twython, TwythonError
-import requests
-requests.packages.urllib3.disable_warnings()
+
+""" the keywords paria provided
+Transit
+Public transportation
+NFTA 
+Bus stop
+Metro station
+and long and lat:
+Coordinates: 42.959428, -78.813205
+Radius: 7.5 km =4.66028mi
+"""
+keyword='Metro'
+geocode='42.959428,-78.813205,4.66mi'
 # Requires Authentication as of Twitter API v1.1
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitter = Twython(keys.APP_KEY, keys.APP_SECRET, keys.OAUTH_TOKEN, keys.OAUTH_TOKEN_SECRET)
 try:
-    search_results = twitter.search(q='NFTA', count=50)
+    search_results = twitter.search(q=keyword, count=100 ,geocode=geocode)
 except TwythonError as e:
     print e
 
+count=0
+f=open("dataset01.txt","a")
+f.write('-------------------'+'for keyword: "'+ keyword +'" ----------------------')
 for tweet in search_results['statuses']:
-    print 'Tweet from @%s Date: %s' % (tweet['user']['screen_name'].encode('utf-8'),tweet['created_at'])
-    print tweet['text'].encode('utf-8'), '\n'
+
+    d1= 'Tweet from @%s Date: %s' % (tweet['user']['screen_name'].encode('utf-8'),tweet['created_at'])
+    d2= tweet['text'].encode('utf-8')
+
+
+    count+=1
+    
+ 
+    f.write('\n')
+    f.write(d1)
+    f.write(d2)
+    f.write('\n')
+    
+    print d1
+    print d2
+
+print count
+f.write('\n')
+f.write("number of tweets found : "+str(count))
+f.write('\n')
+
+f.close()
